b7a12b879323df070abc4746c8c87e873cd971b6
Moskvin Artem <grufruu@yandex.ru>
2024-04-13 17:19:54 +0300

    First Commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
+fun main(args: Array<String>) {
         println("HelloWorld!")
+        println("Program arguments: ${args.joinToString()}")
     }
